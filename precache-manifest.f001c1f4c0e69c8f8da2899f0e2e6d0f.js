d74710c5d7b0bd73e0d8226ed48c84b54ebfe184
unknown <pascalkemetse@gmail.com>
2020-08-12 08:22:27 -0700

    Updates

---
+++ precache-manifest.f001c1f4c0e69c8f8da2899f0e2e6d0f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d38e7799765117c19de850bd595834e",
+    "url": "/React1/index.html"
+  },
+  {
+    "revision": "5e48cca52da50a54a8c5",
+    "url": "/React1/static/css/main.d0a59a3a.chunk.css"
+  },
+  {
+    "revision": "aa6984cd07520a83fd66",
+    "url": "/React1/static/js/2.0a57a6d4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React1/static/js/2.0a57a6d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e48cca52da50a54a8c5",
+    "url": "/React1/static/js/main.cf7d4bb8.chunk.js"
+  },
+  {
+    "revision": "337b6edcc953dd0176b1",
+    "url": "/React1/static/js/runtime-main.7af61781.js"
+  },
+  {
+    "revision": "03cf1e7dffdb7c10344d92777bcd0c8d",
+    "url": "/React1/static/media/image (1).03cf1e7d.jpg"
+  },
+  {
+    "revision": "16d95b89cababbe08a25165ce9080d95",
+    "url": "/React1/static/media/image (2).16d95b89.jpg"
+  },
+  {
+    "revision": "2bcb05cae2d5f9f54e6849c183f3242b",
+    "url": "/React1/static/media/image (3).2bcb05ca.jpg"
+  }
+]);
